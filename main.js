e1b3e8c1df95cd7bae927a2d42a557fb89546fe5
Bruno <luiss3@uw.edu>
2017-03-15 21:45:31 -0700

    add grid

--- main.js
+++ main.js
@@ -2,8 +2,8 @@ window.onload = function() {
 
     var game = new Phaser.Game(800, 600, Phaser.AUTO, '', { preload: preload, create: create, update: update});
     var cells = [];
-    var h = 50;
-    var w = 50;
+    var h = 30;
+    var w = 40;
     var cellsize = 20;
 
     function preload () {
@@ -11,13 +11,17 @@ window.onload = function() {
     }
 
     function create () {
-      var graphics = game.add.graphics(100, 100);
+      var graphics = game.add.graphics(0, 0);
       graphics.lineStyle(2, 0x0000FF, 1);
-      graphics.drawRect(0, 0, 100, 100);
+
+      for(var i = 0;  i < h; i++){
+        for(var j = 0; j < w; j++){
+          graphics.drawRect(i * cellsize, j * cellsize, cellsize, cellsize);
+        }
+      }
+
       window.graphics = graphics;
-      // for(var i = 0;  i < h; i++){
-      //
-      // }
+
     }
 
     function update() {
